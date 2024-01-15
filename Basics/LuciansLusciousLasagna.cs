4c0995a7c3e659f7e6421be9f88b59277c5bb778
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2024-01-15 09:57:26 -0300

    Update LuciansLusciousLasagna.cs

--- Basics/LuciansLusciousLasagna.cs
+++ Basics/LuciansLusciousLasagna.cs
@@ -1,22 +1,22 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    // Tempo que a lasanha deve ficar no forno
 
     public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    // Recebe o tempo que a lasanha está no forno e retorna quanto falta
 
      public int RemainingMinutesInOven(int min){
         return 40 - min;
     }
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    // Tempo de demora tendo em vista o número de camadas (2 min cada)
 
      public int PreparationTimeInMinutes(int camada){
         return camada * 2;
     }
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    // Soma do tempo sendo n as camadas e m os tempo que a lasanha está no forno
 
     public int ElapsedTimeInMinutes(int n, int m ){
         return (n * 2) + m;
