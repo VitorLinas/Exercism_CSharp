20fa8fb527b95d18b59b812d5e8f4fd1912cae35
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2024-01-15 10:27:51 -0300

    Update AnnalynsInfiltration.cs

--- Booleans/AnnalynsInfiltration.cs
+++ Booleans/AnnalynsInfiltration.cs
@@ -4,52 +4,22 @@ static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        if(knightIsAwake == true){
-            return false;
-        }else{
-        return true;
-        }
+      return !knightIsAwake; // Se knightIsAwake for true !knightIsAwake será false
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        if(knightIsAwake == true || archerIsAwake == true || prisonerIsAwake){
-            return true;
-        }else{
-        return false;
-        }
+        return knightIsAwake || archerIsAwake || prisonerIsAwake; // Se ambos forem verdadeiros retornará true
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-      if(archerIsAwake == false && prisonerIsAwake == true){
-          return true;
-          }else{
-      return false;         
-          
-    }
+        return !archerIsAwake  && prisonerIsAwake; // Se !prisonerIsAwake for false e prisonerIsAwake true será retornado true
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        if(knightIsAwake == true && archerIsAwake == true && prisonerIsAwake == true && petDogIsPresent == true){
-            return false;
-        
-        }else if(knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == false && petDogIsPresent == true){
-         return true;
-        }else if(knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true && petDogIsPresent == true){
-         return true;
-        }else if(knightIsAwake == true && archerIsAwake == false && prisonerIsAwake == false && petDogIsPresent == true){
-         return true;
-        }else if(knightIsAwake == true && archerIsAwake == false && prisonerIsAwake == true && petDogIsPresent == true){
-         return true;
-        }else if(knightIsAwake == true && archerIsAwake == true && prisonerIsAwake == false && petDogIsPresent == true){
-         return false;
-        }
-    else if(knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true && petDogIsPresent == false){
-         return true;
-        } 
+            return (petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake); // Se ma das condições forem veraddeiras retorna true
     
-        return false;
-    }
+}
 }
